44b540431d2e27703ed42ff25d88178b1fb99a0d
arttuara <50333463+arttuara@users.noreply.github.com>
2022-09-20 14:21:06 +0300

    Remove raw output

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,7 @@ streamlit.dataframe(fruits_to_show)
 # Display API requests
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Normalizing the json into a readable format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
